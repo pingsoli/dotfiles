60c1a990dfb74418fd5505bd514e8bd975f5fa9c
16.04.3 <pingsoli@163.com>
2018-08-22 13:22:01 +0800

    renew configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,13 +16,13 @@ alias cmeak="cmake"
 alias cmkae="cmake"
 alias camke="cmake"
 alias cmkae="cmake"
-alias maek="make -j4"
-alias meak="make -j4"
-alias amek="make -j4"
-alias amke="make -j4"
-alias mkae="make -j4"
-alias mak="make  -j4"
-alias kmae="make -j4"
+alias maek="make"
+alias meak="make"
+alias amek="make"
+alias amke="make"
+alias mkae="make"
+alias mak="make"
+alias kmae="make"
 alias sl="ls"
 alias s="ls"
 
@@ -30,25 +30,25 @@ set -gx FISH_PATH $HOME/.config/fish
 
 set fish_greeting
 
-# pyenv (python version manager)
+## pyenv (python version manager)
 set -gx PYENV $HOME/.pyenv
 set -gx PATH $PYENV/bin $PATH
 source (pyenv init - | psub)
 
-# ag (searcher tool), Ack.vim based on Ag.
+## ag (searcher tool), Ack.vim based on Ag.
 set -gx AG /usr/local/ag
 set -gx PATH $AG/bin $PATH
 
-# Fzf remove duplicate history
+## Fzf remove duplicate history
 # set -g hist_ignore_dups
 # set -x FZF_DEFAULT_COMMAND "ag -g \"\""
 # set -x FZF_CTRL_T_COMMAND "ag -g \"\""
 
-# Set clang as default compiler.
-set -gx CC /usr/bin/clang
-set -gx CXX /usr/bin/clang++
+## Set clang as default compiler.
+# set -gx CC /usr/bin/clang
+# set -gx CXX /usr/bin/clang++
 
-# Java environment
+## Java environment
 set -gx JAVA_HOME /usr/local/jdk1.8
 set -gx PATH $JAVA_HOME/bin $PATH
 
